f915809bf2b7c60258c3302485ed6693c3f96a92
Haroon Wajid <130307173+haroonwajid@users.noreply.github.com>
2024-08-14 23:24:43 +0500

    Add files via upload

---
+++ images/sum.py
@@ -0,0 +1,9 @@
+def addition():
+    first = int(input("enter teh first number: "))
+    second = int(input("enter the second number: "))
+    
+    sum= first+ second
+    return sum 
+
+
+print(addition())
